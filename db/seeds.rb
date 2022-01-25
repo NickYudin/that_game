6acf88edc60dfcc3753cf886e1a6ace5ed08f1a1
Nick Yudin <metalworldowner@gmail.com>
2022-01-25 18:25:09 +0300

    change seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+  roles = Role.create([{roles: 'admin'}, {roles: 'user'}])
   users = User.create([{email: 'metalworldowner@gmail.com', password: '123456', role_id: 1}, {email: 'nik6594@mail.ru', password: '123456'}])
-  Monster.create([name: 'Gobbo', power: 13], [name: 'Red snake', power: 17])
-  Room.create([description: 'Dark cave', monster_id: 1], [description: 'Underground forrest', monster_id: 2])
+  monsters = Monster.create([{name: 'Gobbo', power: 13}, {name: 'Red snake', power: 17}])
+  rooms = Room.create([{description: 'Dark cave', monster_id: 1}, {description: 'Underground forrest', monster_id: 2}])
