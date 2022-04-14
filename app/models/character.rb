1628ce20d2a04bc90bd2dca4b8af4ccd8bf525b1
Nick Yudin <metalworldowner@gmail.com>
2022-04-14 10:52:58 +0300

    correct level up method

--- app/models/character.rb
+++ app/models/character.rb
@@ -40,7 +40,8 @@ class Character < ApplicationRecord
     @level = level
     if experiense >= ExperienceRequirement.find_by(level: (@level + 1))[:experience]
       @level += 1
-      update(level: @level)
+      hp = max_health + HitIncrease.call(self)
+      update(level: @level, max_health: hp)
       ChatMessage.call("Level up! Now you are at #{@level} level!")
       update_features
     end
